7f2725859144814d8772f8f0c9d03b66dc77ea23
KonstantinSmart <109225721+KonstantinSmart@users.noreply.github.com>
2023-10-14 13:40:39 +0800

    Add files via upload

---
+++ web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'HomeController@index');
+Route::get('/test', 'HomeController@test');
+Route::get('/test2', 'Test\TestController@index');
+Route::get('/page/{slug}', 'PageController@show');
+
+Route::resource('/admin/posts', 'PostController', ['parameters' => [
+    'posts' => 'slug',
+]]);
+
+
+Route::fallback(function () {
+//    return redirect()->route('home');
+    abort(404, 'Oops! Page not found...');
+});
+
